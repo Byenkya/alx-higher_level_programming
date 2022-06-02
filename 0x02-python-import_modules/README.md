9ef06da81621a8ffbbfa168986ff30d21c7d0272
Byenkya <byenkyajoshua1997@gmail.com>
2022-06-02 10:23:31 -0700

    import modules

---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,2 @@
+How to import modules in pyhton both relative and absolute
+
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+        a: first int
+        b: second int
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
