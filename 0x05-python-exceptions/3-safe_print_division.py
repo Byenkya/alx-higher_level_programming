c5457cf02aac56944230b7162bbee6b28436033d
Byenkya <byenkyajoshua1997@gmail.com>
2022-06-20 02:50:00 -0700

    divide 2 numbers changes

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,6 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
+        print("Inside result: {}".format(result))
     except ZeroDivisonError:
         result = None
         print("Inside result: {}".format(result))
