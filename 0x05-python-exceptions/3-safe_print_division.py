f0189c9b7c26a3b6742607186fcf31235d494cb1
Byenkya <byenkyajoshua1997@gmail.com>
2022-06-20 02:43:31 -0700

    divide 2 numbers

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print("Inside result: {}".format(result))
     except ZeroDivisonError:
         result = None
-        print("Inside result: {}".format(result))
     finally:
-        return result
+        print("Inside result: {}".format(response))
+
+    return result
