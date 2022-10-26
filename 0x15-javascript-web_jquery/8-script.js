38be8181fc2f6266c3c4ca0849b2b451b956943a
Byenkya <51961339+Byenkya@users.noreply.github.com>
2022-10-26 15:36:43 +0300

    update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,12 +1,3 @@
-$(function() {
-
-    $.ajax({
-        type: 'GET',
-        url: 'https://swapi-api.hbtn.io/api/films/?format=json',
-        success: function(data) {
-            for (let i = 0; i <= data.results.length; i++) {
-                $('UL#list_movies').append('<li>' + data.results[i].title + '</li>');
-              }
-        }
-            });
+$.get('https://swapi.co/api/films/?format=json', function (data) {
+  $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
 });
