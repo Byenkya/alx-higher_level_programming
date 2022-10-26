2fd773fb5b8a6c3ad80e503dfb933f54fe196882
Byenkya <51961339+Byenkya@users.noreply.github.com>
2022-10-26 15:30:10 +0300

    update script 3.js

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,5 +1,3 @@
-$(function() {
-    $('DIV#red_header').on('click', function() {
-        $('header').addClass('red');
-    });
-});
+$('DIV#red_header').click(function () {
+    $('HEADER').addClass('red');
+  });
